1d509fd1dfaebd1338d323f9dace4659f27c5e71
mbickford227 <mjacksonbro01@indianatech.net>
2026-05-03 19:13:08 -0400

    Add assignments

---
+++ CS3700 M1A2.java
@@ -0,0 +1,62 @@
+// Java code to illustrate Addition operator
+
+class Addition {
+    public static void main(String[] args)
+    {
+        // initializing variables
+        int num1 = 10, num2 = 20, sum = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // adding num1 and num2
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+    }
+}
+class Subtraction {
+    public static void main(String[] args)
+    {
+        // initializing variables
+        int num1 = 20, num2 = 10, sub = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // subtracting num1 and num2
+        sub = num1 - num2;
+        System.out.println("Subtraction = " + sub);
+    }
+}
+class Multiplication {
+    public static void main(String[] args)
+    {
+        // initializing variables
+        int num1 = 20, num2 = 10, mult = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Multiplying num1 and num2
+        mult = num1 * num2;
+        System.out.println("Multiplication = " + mult);
+    }
+}
+class Division {
+    public static void main(String[] args)
+    {
+        // initializing variables
+        int num1 = 20, num2 = 10, div = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Dividing num1 and num2
+        div = num1 / num2;
+        System.out.println("Division = " + div);
+    }
+}
